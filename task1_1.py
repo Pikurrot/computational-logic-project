e01085e96bb358506113fe5d0c106b5e292af7a1
Eric Lopez <90217719+Pikurrot@users.noreply.github.com>
2022-10-22 07:41:59 +0000

    Add comments in functions

--- task1_1.py
+++ task1_1.py
@@ -1,6 +1,7 @@
 from task0 import symbols, all_symbols, is_letter, is_atomic_sentence, preprocessing_data, sentence_OK
 
 def main_connector_pos(string):
+  # Returns the position of the main connector (the one inside only 1 parentesis). If no main connector, returns None
   open_parentheses = 0
   for i in range(len(string)):
     if open_parentheses == 1 and string[i] in symbols:
@@ -9,12 +10,15 @@ def main_connector_pos(string):
       open_parentheses += 1
     elif string[i] == ")":
       open_parentheses -= 1
+  return None
   
 def divide_sentence(string, connector_pos):
+  # Returns the two parts of the string separated by the main connector, excluding outer parentheses
   return string[1:connector_pos], string[connector_pos+1:-1]
   # ((p&q)|¬r)
 
 def handle_not(string):
+  # Returns the string without the outer ¬ (not)
   for c in string:
     if c == "¬":
       return string[1:]
