6ddd125ce33f350a36994a173ca446718795fbf9
Eric Lopez <90217719+Pikurrot@users.noreply.github.com>
2022-11-04 17:34:18 +0000

    Day4

--- task1_1.py
+++ task1_1.py
@@ -1,7 +1,8 @@
 from task0 import symbols, all_symbols, is_letter, is_atomic_sentence, preprocessing_data, sentence_OK
 
 class Sentence:
-  def __init__(self,value,parent):
+  def __init__(self,value,parent=None):
+    # Each sentence object has 3 parameters: the sentence as a string (value), the sentence of which it comes (parent) and the subsentences that can be formed from the sentences (children)
     self.value = value
     self.parent = parent
     self.children = []
@@ -23,7 +24,9 @@ class Sentence:
     return self.value
   
 def main_connector_pos(string):
-  # Returns the position of the main connector (the one inside only 1 parentesis). If no main connector, returns None
+  # Returns the position of the main connector (the one inside only 1 parentesis). If no main connector, or first character is "¬", returns None
+  if string[0] == "¬":
+    return None
   open_parentheses = 0
   for i in range(len(string)):
     if open_parentheses == 1 and string[i] in symbols:
@@ -49,10 +52,13 @@ def handle_not(string):
   # Returns the string without the outer ¬ (not). If no outer ¬, the string is returned
   if string[0] == "¬":
     return string[1:]
+  elif len(string) >= 2 and string[1] == "¬":
+    return string[2:-1]
   else:
     return string
 
 def sentences_are_atomic(list_sentences):
+  # Returns True if any of the sentences in the list is atomic
   for c in list_sentences:
     if not is_atomic_sentence(c.get_value()):
       return False
@@ -60,34 +66,45 @@ def sentences_are_atomic(list_sentences):
 
 def syntactic_tree(string_founder):
   # Builds the syntactic tree.
-  a = Sentence(string_founder, parent=None)
+  a = Sentence(string_founder)
   list_sentences = [a]
   while not sentences_are_atomic(list_sentences): # repeat until all sentences are atomic
     list_sentences_cousins = []
     for sentence in list_sentences:
-      print("sentence:",sentence)
-      alt_string = handle_not(sentence.value) # alternative string without the ¬
-      print("handle not:",alt_string)
-      if alt_string == sentence.value: # if the sentence has not changed
-        divided_string = divide_by_main_connector(sentence.value) # list of the 2 parts of the divided string
-        print("divided:",divided_string)
-        sentence.add_children(divided_string)
-      else:
+    
+      divided_string = divide_by_main_connector(sentence.value) # list of the 2 
+      if divided_string == sentence.value: # if the sentence has not changed
+        alt_string = handle_not(sentence.value) # alternative string without the ¬ parts of the divided string
         sentence.add_children([alt_string])
+      else:
+        sentence.add_children(divided_string)
       list_sentences_cousins.extend(sentence.get_children()) # add children to list of cousins (otherwise, we end only with the children of one of the parents)
     list_sentences = list_sentences_cousins[:] # update the list with the list of cousins, so the process can be repeated again with each cousin as a parent
-  for s in list_sentences:
-    print(s)
+  return list_sentences
 
 def print_tree(tree_lst):
   # Prints the syntactic tree.
-  pass
+  for atomic_sentence in tree_lst:
+    # Print each atomic sentence in a line, with its branch of parents in its left
+    string = atomic_sentence.get_value()
+    parent = atomic_sentence
+    while parent.get_parent() != None:
+      # Adds the parent of the parent in the left of string
+      parent = parent.get_parent()
+      string = parent.get_value() + "     " + string
+    print(string)
 
 def main_task1_1():
   #string = input("Enter a sentence: ")
-  string = "((p&q)|¬r)"    # works
-  string = "((p&q)|(¬r))"  # error with (¬r)
+  string = "((p&q)|¬r)"
+  string = "¬¬((p&q)|(¬r&q))" #now it works
+  # ================= Things we must do: =================
+  #Also "¬" must always be outside parentheses, if it isn't Sentence is not OK. we can solve this:
+    #task 0 (sentenceis not OK when (¬r))
+    #handle not pos[1] never happens
+    # try/except
+  #Also the syntactic tree stops building when there is a not OK sentence. Managing errors.
+  #
   string = preprocessing_data(string)
-  syntactic_tree(string)
-
-
+  tree_lst = syntactic_tree(string)
+  print_tree(tree_lst)
